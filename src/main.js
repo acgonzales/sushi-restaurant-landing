1daf575aad7060029312da004f01b8858fc244d4
Aaron Gonzales <aaroncgonzales.dev@gmail.com>
2025-03-20 22:15:38 +0800

    feat: navbar responsive

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
 import "./style.css";
+
+const mobileMenuButton = document.getElementById('mobile-menu-trigger');
+const mobileMenu = document.getElementById('mobile-menu');
+
+mobileMenuButton.addEventListener('click', (e) => {
+  e.stopPropagation();
+  mobileMenu.classList.toggle('-translate-x-0');
+  mobileMenu.classList.toggle('-translate-x-full');
+});
+
+document.addEventListener('click', (e) => {
+  if (!mobileMenu.contains(e.target) && mobileMenu.classList.contains('-translate-x-0')) {
+    mobileMenu.classList.remove('-translate-x-0');
+    mobileMenu.classList.add('-translate-x-full');
+  }
+});
+
+mobileMenu.addEventListener('click', (e) => {
+  e.stopPropagation();
+});
