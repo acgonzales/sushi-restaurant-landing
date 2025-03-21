1c164e9c894ec5706f83365eaf1e128e8ceac71e
Aaron Gonzales <aaroncgonzales.dev@gmail.com>
2025-03-21 14:06:23 +0800

    feat: our menu responsive

--- src/main.js
+++ src/main.js
@@ -1,21 +1,31 @@
 import "./style.css";
+import Alpine from "alpinejs";
 
-const mobileMenuButton = document.getElementById('mobile-menu-trigger');
-const mobileMenu = document.getElementById('mobile-menu');
-
-mobileMenuButton.addEventListener('click', (e) => {
-  e.stopPropagation();
-  mobileMenu.classList.toggle('-translate-x-0');
-  mobileMenu.classList.toggle('-translate-x-full');
-});
-
-document.addEventListener('click', (e) => {
-  if (!mobileMenu.contains(e.target) && mobileMenu.classList.contains('-translate-x-0')) {
-    mobileMenu.classList.remove('-translate-x-0');
-    mobileMenu.classList.add('-translate-x-full');
+Alpine.data('navbar', () => ({
+  isOpen: false,
+  openMobileMenu() {
+    this.isOpen = !this.isOpen;
   }
-});
+}));
 
-mobileMenu.addEventListener('click', (e) => {
-  e.stopPropagation();
-});
+Alpine.start();
+
+// const mobileMenuButton = document.getElementById('mobile-menu-trigger');
+// const mobileMenu = document.getElementById('mobile-menu');
+
+// mobileMenuButton.addEventListener('click', (e) => {
+//   e.stopPropagation();
+//   mobileMenu.classList.toggle('-translate-x-0');
+//   mobileMenu.classList.toggle('-translate-x-full');
+// });
+
+// document.addEventListener('click', (e) => {
+//   if (!mobileMenu.contains(e.target) && mobileMenu.classList.contains('-translate-x-0')) {
+//     mobileMenu.classList.remove('-translate-x-0');
+//     mobileMenu.classList.add('-translate-x-full');
+//   }
+// });
+
+// mobileMenu.addEventListener('click', (e) => {
+//   e.stopPropagation();
+// });
