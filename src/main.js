a4997b109b4a0b95536fd6941d86d180a9cd70ce
Aaron Gonzales <aaroncgonzales.dev@gmail.com>
2025-03-21 16:03:51 +0800

    feat: improvements

--- src/main.js
+++ src/main.js
@@ -3,29 +3,75 @@ import Alpine from "alpinejs";
 
 Alpine.data('navbar', () => ({
   isOpen: false,
+  init() {
+    const self = this;
+    const mobileMenu = this.$refs.mobileMenu;
+    const mobileMenuTrigger = this.$refs.mobileMenuTrigger;
+
+    document.addEventListener('click', (e) => {
+      if (!mobileMenu.contains(e.target) && !mobileMenuTrigger.contains(e.target) && self.isOpen) {
+        self.isOpen = false;
+      }
+    });
+
+    mobileMenu.addEventListener('click', (e) => {
+      e.stopPropagation();
+    });
+  },
   openMobileMenu() {
     this.isOpen = !this.isOpen;
   }
 }));
 
+Alpine.data('reservationForm', () => ({
+  firstName: '',
+  lastName: '',
+  date: '',
+  notes: '',
+  errors: {
+    firstName: '',
+    lastName: '',
+    date: '',
+    notes: '',
+  },
+  submitForm() {
+    this.errors.firstName = '';
+    this.errors.lastName = '';
+    this.errors.date = '';
+    this.errors.notes = '';
+
+    let hasErrors = false;
+
+    if (!this.firstName.trim()) {
+      this.errors.firstName = 'First name is required';
+      hasErrors = true;
+    }
+
+    if (!this.lastName.trim()) {
+      this.errors.lastName = 'Last name is required';
+      hasErrors = true;
+    }
+
+    if (!this.date.trim()) {
+      this.errors.date = 'Date is required';
+      hasErrors = true;
+    }
+
+    // if (!this.notes.trim()) {
+    //   this.errors.notes = 'Notes are required';
+    //   hasErrors = true;
+    // }
+
+    if (!hasErrors) {
+      const formData = {
+        firstName: this.firstName,
+        lastName: this.lastName,
+        date: this.date,
+        notes: this.notes
+      };
+      console.log('Form submitted successfully:', formData);
+    }
+  }
+}));
+
 Alpine.start();
-
-// const mobileMenuButton = document.getElementById('mobile-menu-trigger');
-// const mobileMenu = document.getElementById('mobile-menu');
-
-// mobileMenuButton.addEventListener('click', (e) => {
-//   e.stopPropagation();
-//   mobileMenu.classList.toggle('-translate-x-0');
-//   mobileMenu.classList.toggle('-translate-x-full');
-// });
-
-// document.addEventListener('click', (e) => {
-//   if (!mobileMenu.contains(e.target) && mobileMenu.classList.contains('-translate-x-0')) {
-//     mobileMenu.classList.remove('-translate-x-0');
-//     mobileMenu.classList.add('-translate-x-full');
-//   }
-// });
-
-// mobileMenu.addEventListener('click', (e) => {
-//   e.stopPropagation();
-// });
